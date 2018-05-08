a3e2b1e2f6f3f9512ecb162a6bccc3db177ae8be
Anca Dragota <dragota.anca15@gmail.com>
2018-05-08 23:18:03 +0300

    Updated webdriver name.

--- src/test/java/com/ancadragota/crawler/test/main/MainTest.java
+++ src/test/java/com/ancadragota/crawler/test/main/MainTest.java
@@ -25,7 +25,7 @@ public class MainTest {
     @BeforeTest
     public void setUp() {
 
-        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver_win32_2.34\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver_win32_2.34\\chromedriver");
 
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
